f42c63821dc70955a836b938fe1cffd5fbcc0cb4
Igor Surkin <igor.surkin@ivkhk.ee>
2025-10-10 19:58:10 +0300

    начали писать код

---
+++ Game/main.js
@@ -0,0 +1,20 @@
+// Импортируем необходимые модули
+
+const readLineSync = require('readline-sync');
+
+// Контроллер игрового цикла
+let keepPlaying = true;
+
+// Основной цикл меню. Будет продолжаться, пока игрок не захочет выйти
+
+while (keepPlaying) {
+    console.clear
+    console.log("===================================");
+    console.log("=       WELCOME TO THE GAME       =");
+    console.log("===================================");
+    console.log("Which game mode do you want to play");
+
+    const games = [
+        'Guess the Number'
+    ];
+}
